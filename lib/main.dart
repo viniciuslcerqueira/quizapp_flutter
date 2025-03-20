360f18fa6e68af7437d73196c47bfc8544e3341d
Vinicius <viniciusleitecerqueira@gmail.com>
2025-03-20 15:43:42 -0300

    commit inicial

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget {
+  const PerguntaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+
+      ),
+    );
+  }
+}
