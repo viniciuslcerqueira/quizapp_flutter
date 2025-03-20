8c00fb5fffa91c97824ce950977bda6ac2dfef0f
Vinicius <viniciusleitecerqueira@gmail.com>
2025-03-20 16:44:07 -0300

    estrutura basica layout + lista perguntas

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,48 @@ void main() {
 class PerguntaApp extends StatelessWidget {
   const PerguntaApp({super.key});
 
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    //Lista de Perguntas
 
-      ),
+    var perguntaSelecionada = 0;
+
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+      'Qual é o seu nome?'
+    ];
+
+    void responder(){
+      perguntaSelecionada++;
+      print(perguntaSelecionada);
+    }
+
+    return  MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Quiz App!',),
+          ),
+          body: Column(
+            children: [
+              Text(perguntas[perguntaSelecionada]),
+              ElevatedButton(
+                  onPressed: responder,
+                  child: Text('Resposta 1')
+              ),
+              ElevatedButton(
+                  onPressed: responder,
+                  child: Text('Resposta 2')
+              ),
+              ElevatedButton(
+                  onPressed: responder,
+                  child: Text('Resposta 3')
+              ),
+
+            ],
+          ),
+      )
     );
   }
 }
